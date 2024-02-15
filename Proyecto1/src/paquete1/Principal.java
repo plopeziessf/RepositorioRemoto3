6c6d965fc0d63f90b2957dc7ea15b13513181cc3
administrador <administrador@ubuntuvm>
2024-02-15 16:52:38 +0100

    Confirmación 2

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 
 		public static void main(String[] args) {
 			System.out.println("Estamos haciendo control de versiones con Git");
+			System.out.println("Modificación 2");
 		}
 }
